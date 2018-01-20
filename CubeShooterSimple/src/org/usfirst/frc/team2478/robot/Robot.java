bd3bc6ad8e87bc6f655083841b0ab5030f54baae
Westwood Robotics <avuong2962@gmail.com>
2018-01-20 13:02:36 -0700

    merge setup

--- CubeShooterSimple/src/org/usfirst/frc/team2478/robot/Robot.java
+++ CubeShooterSimple/src/org/usfirst/frc/team2478/robot/Robot.java
@@ -14,13 +14,6 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.properties file in the
- * project.
- */
 public class Robot extends IterativeRobot {
 
 	private WPI_TalonSRX masterMotor, slaveMotor;
