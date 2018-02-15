d49a1be80170ab152d622aad98ed3c1dad435276
Alex Vuong <avuong2962@gmail.com>
2018-02-14 19:29:58 -0700

    Update feedtest for actual testing

--- FeedTest/src/main/java/frc/team2478/robot/Robot.java
+++ FeedTest/src/main/java/frc/team2478/robot/Robot.java
@@ -1,5 +1,6 @@
 package frc.team2478.robot;
 
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
@@ -7,37 +8,70 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends IterativeRobot {
     
-    private static final int MASTER_ID = 1;
-    private static final int SLAVE_ID = 2; 
-    private WPI_TalonSRX masterMotor, slaveMotor;
+	public static final double RPM_CONVERSION = 600.0 / 4096.0;
+	public static final double GEARBOX_RATIO = 5.0 / 1.0;
+	
+	private static final int SHOOTER_MASTER_ID = 5;
+	private static final int SHOOTER_SLAVE_ID = 3;
+	private WPI_TalonSRX shooterMasterMotor, shooterSlaveMotor;
+	
+    private static final int FEED_MASTER_ID = 1;
+    private static final int FEED_SLAVE_ID = 2; 
+    private WPI_TalonSRX feedMasterMotor, feedSlaveMotor;
+    
+    private static int c = 0;
 
     @Override
     public void robotInit() {
-        masterMotor = new WPI_TalonSRX(MASTER_ID); // right
-        slaveMotor = new WPI_TalonSRX(SLAVE_ID); // left
-        slaveMotor.follow(masterMotor);
+        feedMasterMotor = new WPI_TalonSRX(FEED_MASTER_ID);
+        feedSlaveMotor = new WPI_TalonSRX(FEED_SLAVE_ID);
+        feedSlaveMotor.follow(feedMasterMotor);
+        
+        shooterMasterMotor = new WPI_TalonSRX(SHOOTER_MASTER_ID);
+        shooterSlaveMotor = new WPI_TalonSRX(SHOOTER_SLAVE_ID);
+        shooterSlaveMotor.follow(shooterMasterMotor);
+        shooterSlaveMotor.setInverted(true);
+        shooterMasterMotor.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 10);
     }
 
+    @Override
+    public void robotPeriodic() {
+        if (SmartDashboard.getBoolean("FEED/SHOOTER RESET", false)) {
+            SmartDashboard.putNumber("SET FEED RPM", 0);
+            SmartDashboard.putNumber("SET SHOOTER RPM", 0);
+            SmartDashboard.putBoolean("FEED/SHOOTER RESET", false);
+        }
+    }
+    
     @Override
     public void disabledInit() {
-    	masterMotor.stopMotor();
-        SmartDashboard.putBoolean("FEED RESET", false);
-    }
-
-    //-----------------------//
-
-    @Override
-    public void disabledPeriodic() {
-        if (SmartDashboard.getBoolean("FEED RESET", false)) {
-            SmartDashboard.putNumber("SET FEED", 0);
-            SmartDashboard.putBoolean("FEED RESET", true);
-        }
+    	c = 0;
+    	feedMasterMotor.stopMotor();
+    	shooterMasterMotor.stopMotor();
+        SmartDashboard.putBoolean("FEED/SHOOTER RESET", false);
     }
 
     @Override
     public void teleopPeriodic() {
-        masterMotor.set(SmartDashboard.getNumber("SET FEED", 0));
-        SmartDashboard.putNumber("CURRENT FEED SPEED", masterMotor.getMotorOutputPercent());
+//        feedMasterMotor.set(SmartDashboard.getNumber("SET FEED RPM", 0));
+//        shooterMasterMotor.set(SmartDashboard.getNumber("SET SHOOTER RPM", 0));
+    	shooterMasterMotor.set(0.5);
+    	if (c > 100) {
+    		feedMasterMotor.set(1);
+    	}
+        SmartDashboard.putNumber("CURRENT VELOCITY", velocityToRpm(shooterMasterMotor.getSelectedSensorVelocity(0)));
+        c++;
+        System.out.println(c);
     }
+
+    //-----------------------//
+    
+    public static double velocityToRpm(double vel) {
+		return (vel / GEARBOX_RATIO) * RPM_CONVERSION;
+	}
+    
+    public static double rpmToVelocity(double rpm) {
+		return (rpm / RPM_CONVERSION) * GEARBOX_RATIO;
+	}
 
 }
